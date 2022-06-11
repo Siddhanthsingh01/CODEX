9d9e0da33ba011604935ef0ce606025061f3c1f5
Siddhanthsingh01 <crsidhant01@gmail.com>
2022-06-11 16:29:57 +0530

    scr
    4.30 date 11/06/22

--- app/src/main/java/com/example/codex/quiz/quiz.java
+++ app/src/main/java/com/example/codex/quiz/quiz.java
@@ -21,7 +21,7 @@ public class quiz extends AppCompatActivity {
 
         list=new ArrayList<>();
         list.add(new modelclass("what is your name","A","B","C","D","B"));
-        list.add(new modelclass("what is your qulification","A","B","C","D","Ans"));
+        list.add(new modelclass("what is your qulification","A","B","C","D","sohail"));
         list.add(new modelclass("what is your age","A","B","C","D","Ans"));
         list.add(new modelclass("what is your number","A","B","C","D","Ans"));
 
